8d40592df887e8006f3f8523229ae92187ffa1fd
moldingtofu <jeremykchang@gmail.com>
2019-02-16 14:52:26 -0500

    tables

--- node/database.js
+++ node/database.js
@@ -9,11 +9,21 @@ const db = new sqlite3.Database('./photos.db', sqlite3.OPEN_READWRITE | sqlite3.
 
 db.run("CREATE TABLE IF NOT EXISTS users(" +
     "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-    ");",
+    "username VARCHAR(20) UNIQUE," +
+    "password VARCHAR(20)" +
+    ");" +
+
+    "CREATE TABLE IF NOT EXISTS photos(" +
+    "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+    "userid INTEGER," +
+    "FOREIGN KEY(userid) REFERENCES users(id)," +
+    ");"
+
+    ,
     function(err) {
       if (err)
         throw err;
-      console.log("Created table if it didn't exist already")
+      console.log("Created tables if it didn't exist already")
     });
 
 module.exports = db;
