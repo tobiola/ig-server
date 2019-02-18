cf9ff57a7c331b74836d2b4efa41f7b3728ee5ab
moldingtofu <jeremykchang@gmail.com>
2019-02-18 01:03:03 -0500

    added caption column to photos

--- node/database.js
+++ node/database.js
@@ -8,22 +8,22 @@ const db = new sqlite3.Database('./instagram.db', sqlite3.OPEN_READWRITE | sqlit
 });
 
 db.run("CREATE TABLE IF NOT EXISTS users(" +
-    "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-    "username VARCHAR(20) UNIQUE," +
-    "password VARCHAR(20)" +
-    ");" +
+  "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+  "username VARCHAR(20) UNIQUE," +
+  "password VARCHAR(20)" +
+  ");" +
 
-    "CREATE TABLE IF NOT EXISTS photos(" +
-    "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-    "userid INTEGER," +
-    "FOREIGN KEY(userid) REFERENCES users(id)," +
-    ");"
-
-    ,
-    function(err) {
-      if (err)
-        throw err;
-      console.log("Created tables if it didn't exist already")
-    });
+  "CREATE TABLE IF NOT EXISTS photos(" +
+  "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+  "userid INTEGER," +
+  "FOREIGN KEY(userid) REFERENCES users(id)," +
+  "caption VARCHAR(200)" +
+  ");"
+  ,
+  function(err) {
+    if (err)
+      throw err;
+    console.log("Created tables if it didn't exist already")
+  });
 
 module.exports = db;
