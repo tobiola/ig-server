76d12035b68721c33b89c50276298ad70a364884
moldingtofu <jeremykchang@gmail.com>
2019-02-18 00:38:04 -0500

    server things

--- node/database.js
+++ node/database.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database('./photos.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+const db = new sqlite3.Database('./instagram.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
   if (err) {
     return console.error(err.message);
   }
--- node/server.js
+++ node/server.js
@@ -8,18 +8,54 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, cache-control');
-    return next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, cache-control');
+  return next();
 });
 
 const port = 8000;
+
 app.use((req, res, next) => {
   console.log('server in use');
   next();
 });
 
-app.use('/', router);
-server = app.listen(port);
-console.log('Express server listening on port %d in %s mode.', port, app.settings.env);
+app.get('/login/:username?:password?', function (req, res) {
+  let username = req.params.username;
+  let password = req.params.password;
+  let query = 'SELECT id FROM users WHERE username=? AND password=?;';
+
+  db.run(query, [username, password], function (err, pictures) {
+    if (err) {
+      console.log(err.message);
+      res.send('error finding user');
+    }
+    else {
+      console.log('logged in');
+      res.send(pictures);
+    }
+  });
+});
+
+app.post('/create/:username?:password?', function (req, res) {
+  let username = req.params.username;
+  let password = req.params.password;
+  let query = 'INSERT INTO users (username, password) VALUES(?,?);';
+
+  db.run(query, [username, password], function (err) {
+    if (err) {
+      console.log('user already exists');
+      res.send('user already exists');
+    }
+    else {
+      console.log('user created');
+    }
+
+    return;
+  });
+});
+
+var server = app.listen(port, function () {
+  console.log('server running at http://localhost:%s', port);
+});
